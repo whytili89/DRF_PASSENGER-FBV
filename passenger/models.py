3ada187113c36c25381dc5c163d48bd7a621bd80
Taeyeon <mindc1891@gmail.com>
2022-02-23 18:46:53 +0900

    Passenger 모델생성

--- passenger/models.py
+++ passenger/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Passenger(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    travel_points = models.IntegerField
+
+    def __str__(self):
+        return self.first_name+self.last_name+self.travel_points
+
+    class Meta:
+        db_table = 'passenger'    
