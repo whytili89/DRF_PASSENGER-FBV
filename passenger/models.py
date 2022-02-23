3f598b15d9dd884e9115d06a815e4a57e632920a
Taeyeon <mindc1891@gmail.com>
2022-02-23 21:13:48 +0900

    views.py 작성

--- passenger/models.py
+++ passenger/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Passenger(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    travel_points = models.IntegerField
+    travel_points = models.IntegerField()
 
     def __str__(self):
         return self.first_name+self.last_name+self.travel_points
